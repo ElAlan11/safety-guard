a54a392ba50982e49e5f4f89cafd20a806adba2e
CarolinaGutierrezS25 <58743673+CarolinaGutierrezS25@users.noreply.github.com>
2023-07-03 12:47:23 -0600

    Access location, camera, ...

--- pages/Home.js
+++ pages/Home.js
@@ -3,16 +3,85 @@ import { StyleSheet, Text, View } from "react-native";
 import BtnContainer from "../components/Home/BtnContainer";
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            isUnsafe: false,
+            location: null
+        };
+    }
+  
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const location = position;
+                console.log(location.coords)
+                this.setState({ location });
+            },
+            error => Alert.alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+        //navigator.geolocation.watchPosition(onLocationActualization, onErrorLocation, options);
+    }
+
+  /*componentDidUpdate(){
+    if(this.isUnsafe){
+        console.log('entro aquii')
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const location = position;
+                console.log(location.coords)
+                this.setState({ location });
+            },
+            error => Alert.alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+    }
+  }*/
+  
+  
+
   render() {
+
+    const handleChangeView = (view) => {
+        const navigation_ = this.context;
+        navigation_.navigate(view, {});
+    }
+
+    const handleLogin = () => {
+        const navigation_ = this.context;
+        navigation_.navigate('Home', {});
+    }
+
+    /*const onLocationActualization = position => {
+        // Aquí ya tenemos la ubicación ;)
+        console.log(position)
+        const location = position;
+        this.setState({ location });
+    }
+
+    const onErrorLocation = err => {
+        console.log("Error obteniendo ubicación: ", err);
+    }
+
+    const options = {
+        enableHighAccuracy: true, // Alta precisión
+        maximumAge: 0, // No queremos caché
+        timeout: 5000 // Esperar solo 5 segundos
+    };*/
+
+    //idWatcher = navigator.geolocation.watchPosition(onLocationActualization, onErrorLocation, options);
+
     const toggle = () => {
-      this.setState({ open: !this.state.open });
+        this.setState({ open: !this.state.open });
     };
+
+    const handlePressHelp = () => {
+        console.log(!this.state.isUnsafe)
+        this.setState({ isUnsafe: !this.state.isUnsafe });
+    };
+
 
     return (
       <View style={styles.mainView}>
